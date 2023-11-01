315112972fefac5a812e4d3b0a69d59f80ac9cc9
= <=>
2023-11-01 09:30:20 +0100

    SSL verification logic

--- api.py
+++ api.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 from OpenSSL import SSL
 import requests
+import ssl
 import idna
 
 app = Flask(__name__)
 
 def check_ssl(hostname):
-    #TODO: Logic to validate SSL certificate
-    
-    return True
+    #Logic to validate SSL certificate
+    try:
+        ssl.get_server_certificate((hostname, 443))
+        return True
+    except Exception:
+        return False
 
 def check_russian_chars(domain):
     try:
@@ -19,12 +23,12 @@ def check_russian_chars(domain):
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    data = request.json
+    data = request.json()
     url = data.get('url')
     hostname = data.get('hostname')
 
     #TODO: Check URL against a database or some other logic
-    
+    response = requests.get(url)
 
     # Check SSL certificate
     ssl_valid = check_ssl(hostname)
