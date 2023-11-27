ec8675a16c3b4ebba9d3f19fb004e6f6dbddd376
MoroccanTea <hamza.essad@ynov.com>
2023-11-27 04:38:53 +0100

    Fixed OAUTH bug - missing files

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,11 +1,12 @@
-// Display version
-document.addEventListener('DOMContentLoaded', function() {
+// Display version - DISABLED TO BE USED LATED IN SETTINGS
+/*document.addEventListener('DOMContentLoaded', function() {
     const manifestData = chrome.runtime.getManifest();
     const version = manifestData.version;
     document.getElementById('version').textContent = `PhishMeNot V.${version}`;
-  });
+  });*/
+
 
 // Login button
-document.getElementById('loginButton').addEventListener('click', function() {
+document.getElementsByClassName('g-sign-in-button')[0].addEventListener('click', function() {
   chrome.runtime.sendMessage({action: "login"});
 });
